81bab2994a3c97d2fa816413925f6669a7ee96af
dddaavo <deardeivid@hotmail.com>
2025-09-09 21:57:09 -0300

    Desafio #3 - Proyecto completo con requerimientos obligatorios

---
+++ entrega-3-duarte-david/entities/enemies/Turret.gd
@@ -0,0 +1,34 @@
+extends Sprite2D
+
+@onready var fire_position: Node2D = $FirePosition
+@onready var fire_timer: Timer = $FireTimer
+
+@export var projectile_scene: PackedScene
+
+var projectile_container: Node
+var target:Node2D
+
+func _ready():
+	fire_timer.connect("timeout", fire_at_player)
+
+func initialize(turret_pos: Vector2, projectile_container: Node) -> void:
+	global_position = turret_pos
+	self.projectile_container = projectile_container
+
+func fire_at_player() -> void:
+	var proj_instance = projectile_scene.instantiate()
+	proj_instance.initialize(
+		projectile_container,
+		fire_position.global_position,
+		fire_position.global_position.direction_to(target.global_position)
+	)
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		target = body
+		fire_timer.start()
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body == target:
+		target = null
+		fire_timer.stop()
